83ea412398e38d74f4f6beb597e58329f941577f
JomiC <jomic1010@naver.com>
2020-08-31 03:39:53 +0900

    MyTodos 생성and 레이아웃 구성중

---
+++ MyTodos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTodos
+//
+//  Created by JomiC on 2020/08/31.
+//  Copyright © 2020 JomiC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
